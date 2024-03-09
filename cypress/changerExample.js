f899b4b80dde6666b1574dd014c4658381c9515a
Roman Stas <romastas1996@gmail.com>
2024-03-10 01:20:55 +0200

    add way to file

--- cypress/changerExample.js
+++ cypress/changerExample.js
@@ -1,13 +1,14 @@
 //Get data from з JSON
 const fs = require('fs');
-const example = JSON.parse(fs.readFileSync('cypress/fixtures/example.json', 'utf8'));
+const wayToFile = 'cypress/fixtures/example.json'
+const example = JSON.parse(fs.readFileSync(wayToFile, 'utf8'));
 
 
 //Change data for test case
-example.name = "John2";
+example.name = "John3";
 
 
 
 
 //Save data to Json file
-fs.writeFileSync('cypress/fixtures/example.json', JSON.stringify(example), 'utf8');
+fs.writeFileSync(wayToFile, JSON.stringify(example), 'utf8');
